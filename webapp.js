9b7120ab9b0f9e863f3557b41745868c93f37fd7
hentschellj <hentschellj@gmail.com>
2020-03-05 13:55:18 -0600

    Path name being acquired

--- webapp.js
+++ webapp.js
@@ -1,9 +1,11 @@
-const http = require('http');
+const http  = require('http');
+const url   = require('url');
 
 http.createServer(onRequest).listen(8888);
-console.log('Server Running');
 
 function onRequest(request, response) {
+  const pathName = url.parse(request.url).pathname;
+  console.log('Path Name ' + pathName);
   response.writeHead(200);
   response.write('Hello!');
   response.end();
